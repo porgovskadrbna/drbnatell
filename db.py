d27fa884cf10a029cae8fc4c7ed3b207f1a6d111
Michal Janočko <michal@janocko.com>
2021-12-15 10:18:38 +0100

    🗃️ Init the database

---
+++ db.py
@@ -0,0 +1,6 @@
+TORTOISE_ORM = {
+    "connections": {
+        "default": "postgres://drbnatell:effort_unmarked_unexposed@michaljanocko.com:5432/drbnatell_dev",
+    },
+    "apps": {"models": {"models": ["aerich.models", "models.tells"]}},
+}
